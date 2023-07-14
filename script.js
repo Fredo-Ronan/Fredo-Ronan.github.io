892287c96e3bbbbd73505438b836ae8c824dea4d
Fredo Code <128687596+Fredo-Ronan@users.noreply.github.com>
2023-07-14 14:23:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const hamburgerWrapper = document.querySelector('.hamburgerWrapper input');
+const navbarSlideContainer = document.querySelector('.navBtnContainer');
+const body = document.querySelector('body');
+
+// handle hamburger menu icon click
+hamburgerWrapper.addEventListener('click', function(){
+    navbarSlideContainer.classList.toggle('slide');
+    body.classList.toggle('overflowHide');
+});
+
+const navbarDiv = document.querySelector('.navbarDiv');
+
+// handle navbar change style
+window.addEventListener('scroll', function(){
+    navbarDiv.classList.toggle('sticky', this.window.scrollY > 20);
+    navbarSlideContainer.classList.add('sticky', this.window.scrollY > 20);
+
+    // to remove the sticky class to make the sidebar more transparent style
+    // when the page is on top or scrolled to the very top of the page
+    if(this.window.scrollY == 0){
+        navbarSlideContainer.classList.remove('sticky');
+    }
+})
