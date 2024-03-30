7823f340c3086e0fa7bccebc625478a4fcf6a2e5
Fredo-Ronan <fredocode06@gmail.com>
2024-03-30 22:35:41 +0700

    refactoring content data to github repo page

--- js/data/github-repos-constant.js
+++ js/data/github-repos-constant.js
@@ -5,6 +5,125 @@
 
 export const github_repos = [
     {
-        
+        github_repo_title: "Live Chat Web App",
+        technologies: [
+            'Socket IO',
+            'Express JS',
+            'Node JS',
+            'Javascript',
+            'HTML5',
+            'CSS3'
+        ],
+        state: 'Stable',
+        github_repo_description: "Multi user web chat that support feature like send voice message, photos, and many more.",
+        github_repo_link: "https://github.com/Fredo-Ronan/Live-Chat-Web",
+        support_os: "Windows, Linux, MacOS"
+    },
+    {
+        github_repo_title: "Snake Game in C",
+        technologies: [
+            'C'
+        ],
+        state: "Stable",
+        github_repo_description: "Snake game that made without using graphics.h C library.",
+        github_repo_link: "https://github.com/Fredo-Ronan/Snake-Game-Console-without-graphics.h",
+        support_os: "Windows"
+    },
+    {
+        github_repo_title: "RSA Encryption",
+        technologies: [
+            "Javascript",
+            "Python",
+            "C++",
+            "Kotlin"
+        ],
+        state: "Stable",
+        github_repo_description: `A collection of my custom made library of RSA Encryption in several programming languages to help to do
+        some RSA Encryption Algorithm without re-coding it again.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/RSA_Encryption_Algorithm",
+        support_os: "Windows, Linux, MacOS"
+    },
+    {
+        github_repo_title: "Simple Weather App",
+        technologies: [
+            "Kotlin"
+        ],
+        state: "Stable",
+        github_repo_description: `This is a Simple Weather Android App build using Kotlin that fetch forecast data from BMKG's open source data through my custom
+        API.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/Simple_Weather_App",
+        support_os: "Android"
+    },
+    {
+        github_repo_title: "Simple Neural Network in C",
+        technologies: [
+            "C"
+        ],
+        state: "Stable",
+        github_repo_description: `This is a simple implementation of Simple Supervised Learning Neural Network Example using C programming language.`,
+        github_repo_link: 'https://github.com/Fredo-Ronan/Simple_C_Neural_Network',
+        support_os: "Windows"
+    },
+    {
+        github_repo_title: "Simple Person Detection in C++",
+        technologies: [
+            "C++"
+        ],
+        state: "Stable",
+        github_repo_description: `This is a simple program written in C++ programming language to detect person from camera/webcam input.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/Simple_Person_Detection",
+        support_os: "Linux"
+    },
+    {
+        github_repo_title: `Jawa Lang <h4 style="font-size: 15px; display: inline; font-style: italic; opacity: 0.8;">(Under development)</h4>`,
+        technologies: [
+            "C"
+        ],
+        state: "Under Development",
+        github_repo_description: `Just for fun project creating the new programming language inspire by Javanese Language or "Bahasa Jawa" for the syntax of
+        this programming language.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/Jawa-lang",
+        support_os: "Windows"
+    },
+    {
+        github_repo_title: "C Header File Collection",
+        technologies: [
+            "C"
+        ],
+        state: "Stable",
+        github_repo_description: `Collection of .h files or C library header files that i've made to solve certain purposes.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/C_Header_Files_Collection",
+        support_os: "Windows, Linux"
+    },
+    {
+        github_repo_title: "Java Class Utilities",
+        technologies: [
+            "Java"
+        ],
+        state: "Under Development",
+        github_repo_description: `Collection of my custom made Java Class to solve particular problem define by the class name by using the methods
+        available in a certain class.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/Java-Class-Utilities",
+        support_os: "Windows"
+    },
+    {
+        github_repo_title: "Custom Bash Script",
+        technologies: [
+            "Bash"
+        ],
+        state: "Stable",
+        github_repo_description: `Collection of my custom made bash script or shell script to automatically run particular operation on Linux Operating System.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/Custom-Bash-Scripts",
+        support_os: "Linux"
+    },
+    {
+        github_repo_title: "Windows Automation Script",
+        technologies: [
+            "Powershell"
+        ],
+        state: "Stable",
+        github_repo_description: `Collection of my custom made batch script and powershell script to run particular operation automatically on Windows Operating System.`,
+        github_repo_link: "https://github.com/Fredo-Ronan/Windows-Script-Automation",
+        support_os: "Windows"
     }
 ]
